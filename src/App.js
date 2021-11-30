889371f7ef42cceca20b96a221d6d98a397e25d6
Vitor Nunes <stecks10@gmail.com>
2021-11-30 12:20:46 -0300

    Layout e containers

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
+import React from 'react';
 import { FormularioCadastro } from './components/FormularioCadastro';
+import { Container } from '@material-ui/core';
 
 function App() {
   return (
-    <>
+    <Container component="article" maxWidth="sm">
       <h1>Formulario Cadastro</h1>
       <FormularioCadastro />
-    </>
+    </Container>
   );
 }
 
--- src/components/FormularioCadastro/index.jsx
+++ src/components/FormularioCadastro/index.jsx
@@ -5,14 +5,27 @@ import TextField from '@material-ui/core/TextField';
 export function FormularioCadastro() {
   return (
     <form>
-      <TextField id="nome" label="Nome" variant="outlined" fullWidth />
+      <TextField
+        id="nome"
+        label="Nome"
+        variant="outlined"
+        fullWidth
+        margin="normal"
+      />
       <TextField
         id="sobrenome"
         label="Sobrenome"
         variant="outlined"
         fullWidth
+        margin="normal"
       />
-      <TextField id="cpf" label="CPF" variant="outlined" fullWidth />
+      <TextField
+        id="cpf"
+        label="CPF"
+        variant="outlined"
+        fullWidth
+        margin="normal"
+      />
 
       <label>Promoções</label>
       <input type="checkbox" />
