e9a71a1a8ed1d2fdd7f9ffd9a30e726c819ece13
Vitor Nunes <stecks10@gmail.com>
2021-12-02 17:58:09 -0300

    README 🚀🚀

--- src/components/FormularioCadastro/index.jsx
+++ src/components/FormularioCadastro/index.jsx
@@ -14,6 +14,7 @@ export function FormularioCadastro({ aoEnviar, validarCPF }) {
       aoEnviar={(e) => {
         e.preventDefault();
         aoEnviar({ nome, sobrenome, cpf, novidades, promocoes });
+        console.log(nome, sobrenome, cpf, novidades, promocoes);
       }}
     >
       <TextField
