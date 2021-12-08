30583bddefae3c163a95812e4212f08566eb834f
Vitor Nunes <stecks10@gmail.com>
2021-12-08 13:40:36 -0300

    Problemas com o SwitchCase

--- src/components/FormularioCadastro/index.jsx
+++ src/components/FormularioCadastro/index.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import { Typography } from '@material-ui/core';
 import { DadosEntrega } from '../DadosEntrega';
 import { DadosPessoais } from '../DadosPessoais';
 import { DadosUsuario } from '../DadosUsuario';
@@ -7,21 +6,15 @@ import { DadosUsuario } from '../DadosUsuario';
 export function FormularioCadastro({ aoEnviar, validarCPF }) {
   const [etapaAtual, setEtapaAtual] = useState(0);
 
+  const formulario = [
+    <DadosUsuario aoEnviar={proximo} />,
+    <DadosPessoais aoEnviar={proximo} validarCPF={validarCPF} />,
+    <DadosEntrega aoEnviar={aoEnviar} />,
+  ];
+
   function proximo() {
     setEtapaAtual(etapaAtual + 1);
   }
 
-  function formularioAtual(etapa) {
-    switch (etapa) {
-      case 0:
-        return <DadosUsuario aoEnviar={proximo} />;
-      case 1:
-        return <DadosPessoais aoEnviar={proximo} validarCPF={validarCPF} />;
-      case 2:
-        return <DadosEntrega aoEnviar={aoEnviar} />;
-      default:
-        return <Typography>Erro ao selecionar formulario</Typography>;
-    }
-  }
-  return <>{formularioAtual(etapaAtual)}</>;
+  return <>{formulario(etapaAtual)}</>;
 }
