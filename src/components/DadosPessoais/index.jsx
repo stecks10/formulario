261feb739b871941bfbfd1b8db307a037c13866f
Vitor Nunes do Nascimento <stecks10@gmail.com>
2022-01-10 20:47:17 -0300

    finalizado

--- src/components/DadosPessoais/index.jsx
+++ src/components/DadosPessoais/index.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useContext } from 'react';
 import { TextField, Button, Switch, FormControlLabel } from '@material-ui/core';
 import ValidacoesCadastro from '../../context/ValidacoesCadastro';
+import useErros from '../../hooks/useErros';
 
 export function DadosPessoais({ aoEnviar }) {
   const [nome, setNome] = useState('');
@@ -8,27 +9,10 @@ export function DadosPessoais({ aoEnviar }) {
   const [cpf, setCpf] = useState('');
   const [promocoes, setPromocoes] = useState(true);
   const [novidades, setNovidades] = useState(false);
-  const [erros, setErros] = useState({
-    cpf: { valido: true, texto: '' },
-    nome: { valido: true, texto: '' },
-  });
-
   const validacoes = useContext(ValidacoesCadastro);
 
-  function validarCampos(event) {
-    const { name, value } = event.target;
-    const novoEstado = { ...erros };
-    novoEstado[name] = validacoes[name](value);
-    setErros(novoEstado);
-  }
-  function possoEnviar() {
-    for (let campo in erros) {
-      if (!erros[campo].valido) {
-        return false;
-      }
-    }
-    return true;
-  }
+  const [erros, validarCampos, possoEnviar] = useErros(validacoes);
+
   return (
     <form
       onSubmit={(event) => {
--- src/components/FormularioCadastro/index.jsx
+++ src/components/FormularioCadastro/index.jsx
@@ -4,7 +4,7 @@ import { DadosPessoais } from '../DadosPessoais';
 import { DadosUsuario } from '../DadosUsuario';
 import { Step, StepLabel, Stepper, Typography } from '@material-ui/core';
 
-export function FormularioCadastro({ aoEnviar, validacoes }) {
+export function FormularioCadastro({ aoEnviar }) {
   const [etapaAtual, setEtapaAtual] = useState(0);
   const [dadosColetados, setDados] = useState({});
 
@@ -15,9 +15,9 @@ export function FormularioCadastro({ aoEnviar, validacoes }) {
   });
 
   const formularios = [
-    <DadosUsuario aoEnviar={coletarDados} validacoes={validacoes} />,
-    <DadosPessoais aoEnviar={coletarDados} validacoes={validacoes} />,
-    <DadosEntrega aoEnviar={coletarDados} validacoes={validacoes} />,
+    <DadosUsuario aoEnviar={coletarDados} />,
+    <DadosPessoais aoEnviar={coletarDados} />,
+    <DadosEntrega aoEnviar={coletarDados} />,
     <Typography variant="h5">Obrigado pelo Cadastro!</Typography>,
   ];
 
